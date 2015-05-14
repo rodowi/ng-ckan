c45c0224cc77f70419502552734855f1a3ea57cc
Carlos Cessa <carlos@pixative.com>
2015-05-13 20:47:47 -0500

    Group unit test
    - Modified unit test describer for the GroupCtrl to load the group
    data from the mock json file
    - Renamed group mock file to improve readability

--- test/spec/controllers/group.js
+++ test/spec/controllers/group.js
@@ -6,17 +6,34 @@ describe('Controller: GroupCtrl', function () {
   beforeEach(module('ngCkanApp'));
 
   var GroupCtrl,
-    scope;
+      rootScope,
+      scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
+  beforeEach(inject(function ($controller, $rootScope, $q) {
+    // Set the path for the tests fixtures
+    jasmine.getJSONFixtures().fixturesPath  = 'base/test/mock';
+
+    rootScope = $rootScope;
+    scope     = $rootScope.$new();
     GroupCtrl = $controller('GroupCtrl', {
-      $scope: scope
+      $scope        : scope,
+      $routeParams  : {
+        groupId     : 1
+      },
+      ckanService   : {
+        showGroup   : function ( groupId ) {
+          var data      = getJSONFixture( 'group.json' );
+          var deferred  = $q.defer();
+          deferred.resolve( data );
+          return deferred.promise;
+        }
+      }
     });
   }));
 
   it('should attach a list of group\'s datasets to the scope', function () {
+    rootScope.$apply();
     expect(scope.group.packages.length).toBe(1);
   });
 });
