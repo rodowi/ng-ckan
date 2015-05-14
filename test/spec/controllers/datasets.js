90b477eac26c4ed9d615b3456fe72d60ab7eda36
Carlos Cessa <carlos@pixative.com>
2015-05-13 20:39:03 -0500

    Datasets unit tests
    - Modified unit test describer to verify the DatasetsCtrl
    functionality
    - Created mock json file with a server datasets response

--- test/spec/controllers/datasets.js
+++ test/spec/controllers/datasets.js
@@ -2,21 +2,35 @@
 
 describe('Controller: DatasetsCtrl', function () {
 
+  var DatasetsCtrl,
+      rootScope,
+      scope;
+
   // load the controller's module
   beforeEach(module('ngCkanApp'));
 
-  var DatasetsCtrl,
-    scope;
-
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    DatasetsCtrl = $controller('DatasetsCtrl', {
-      $scope: scope
+  beforeEach(inject(function ($controller, $rootScope, $q) {
+    // Set the path for the tests fixtures
+    jasmine.getJSONFixtures().fixturesPath  = 'base/test/mock';
+
+    rootScope     = $rootScope;
+    scope         = $rootScope.$new();
+    DatasetsCtrl  = $controller('DatasetsCtrl', {
+      $scope        : scope,
+      ckanService   : {
+        listDatasets  : function ( start, query ) {
+          var data      = getJSONFixture( 'datasets.json' );
+          var deferred  = $q.defer();
+          deferred.resolve({ datasets : data.result.results, resultsCount : data.result.count });
+          return deferred.promise;
+        }
+      }
     });
   }));
 
   it('should attach a list of datasets to the scope', function () {
-    expect(scope.datasets.length).toBe(3);
+    rootScope.$apply();
+    expect(scope.datasets.length).toBe(10);
   });
 });
