8a3f14051abf8dd169d89c69fbe1ac706b3cecb9
Carlos Cessa <carlos@pixative.com>
2015-05-15 01:46:02 -0500

    Group E2E - Modified group describer to load data from a mock JSON file - Changed URL to math the one used for resources in the application

--- test/e2e/group.js
+++ test/e2e/group.js
@@ -2,30 +2,44 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('group', function() {
+var data  = require( '../mock/group.json' );
+
+describe( 'group', function() {
   var datasetList;
 
-  beforeEach(function() {
-    browser.get('/#/groups/foo');
-    datasetList = element.all(by.repeater('dataset in group.packages'));
+  beforeEach( function() {
+    browser.addMockModule( 'ngCkanApp', function () {
+      var data  = arguments[0];
+
+      angular.module( 'ngCkanApp' )
+        .service( 'ckanService', [ '$q', function ( $q ) {
+          this.showGroup  = function ( id ) {
+            var deferred  = $q.defer();
+            deferred.resolve( data );
+            return deferred.promise;
+          }
+        }]);
+    }, data );
+
+    browser.get( '/#/grupos/rmx' );
+    datasetList = element.all( by.repeater( 'dataset in group.packages' ) );
   });
 
-  it('should render a group when user navigates to /#/groups/foo', function() {
-    expect(element.all(by.css('.ng-binding')).first().getText()).
-      toMatch(/desastres naturales/);
+  it( 'should render a group when user navigates to /#/grupos/rmx', function() {
+    expect( element.all( by.css( '.ng-binding' ) ).first().getText() ).
+      toBe( 'ReconstrucciónMX' );
   });
 
-  it('should list its datasets', function() {
-    expect(datasetList.count()).toEqual(1);
-    expect(datasetList.first().getText()).toMatch('FONDEN');
+  it( 'should list its datasets', function() {
+    expect( datasetList.count() ).toEqual( 1 );
+    expect( datasetList.first().getText() ).toMatch( 'FONDEN' );
   });
 
-  it('should link to dataset\'s detailed information', function() {
+  it( 'should link to dataset\'s detailed information', function() {
     // Click on a dataset link
-    element(by.binding('dataset.title')).click();
+    element( by.binding( 'dataset.title' ) ).click();
     // Find a download link
-    var content = element(by.css('[ng-view]')).getText();
-    expect(content).toMatch(/Datos y recursos/);
+    var content = element( by.css( '[ng-view]' ) ).getText();
+    expect( content ).toMatch( /Datos y recursos/ );
   });
-
 });
