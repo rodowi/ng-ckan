7304aa94fc31ddd2e3ff282030bdcd4710df9163
Carlos Cessa <carlos@pixative.com>
2015-05-15 02:02:07 -0500

    Organization E2E
    - Modified organization describer to load data from a mock JSON file
    - Changed URL to match the one used for organization in the
    application

--- test/e2e/organization.js
+++ test/e2e/organization.js
@@ -2,30 +2,45 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('organization', function() {
+var data  = require( '../mock/organization.json' );
+
+describe( 'organization', function() {
   var datasetList;
 
-  beforeEach(function() {
-    browser.get('/#/organizations/foo');
-    datasetList = element.all(by.repeater('dataset in organization.packages'));
+  beforeEach( function() {
+    browser.addMockModule( 'ngCkanApp', function () {
+      var data  = arguments[0];
+
+      angular.module( 'ngCkanApp' )
+        .service( 'ckanService', [ '$q', function ( $q ) {
+          this.showOrganization = function ( id ) {
+            var deferred  = $q.defer();
+            deferred.resolve( data );
+            return deferred.promise;
+          }
+        }]);
+    }, data );
+
+    browser.get( '/#/instituciones/conapo' );
+    datasetList = element.all( by.repeater( 'dataset in organization.packages' ) );
   });
 
-  it('should render an organization when user navigates to /#/organizations/foo', function() {
-    expect(element.all(by.css('.ng-binding')).first().getText()).
-      toMatch(/Consejo Nacional de Población/);
+  it( 'should render an organization when user navigates to /#/instituciones/*', function() {
+    expect( element.all( by.css( '.ng-binding' ) ).first().getText() ).
+      toBe( "CONAPO" );
   });
 
-  it('should list its datasets', function() {
-    expect(datasetList.count()).toEqual(1);
-    expect(datasetList.first().getText()).toMatch('población');
+  it( 'should list its datasets', function() {
+    expect( datasetList.count() ).toEqual( 1 );
+    expect( datasetList.first().getText() ).toMatch( 'población' );
   });
 
-  it('should link to dataset\'s detailed information', function() {
+  it( 'should link to dataset\'s detailed information', function() {
     // Click on a dataset link
-    element(by.binding('dataset.title')).click();
+    element(by.binding( 'dataset.title' ) ).click();
     // Find a download link
-    var content = element(by.css('[ng-view]')).getText();
-    expect(content).toMatch(/Datos y recursos/);
+    var content = element( by.css( '[ng-view]' ) ).getText();
+    expect( content ).toMatch( /Datos y recursos/ );
   });
 
 });
