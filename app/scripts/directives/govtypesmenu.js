e3c44759fd4511a8706a73bd72ec8c4f5adf4721
Carlos Cessa <carlos@pixative.com>
2015-05-21 16:17:11 -0500

    Government Datasets Count
    - Modified gov-types-menu directive to include a count of the datasets
    available per government level
    - Added countDatasets method to the ckanService to only retrieve the
    number of records for a given query
    - Altered directive template to include scope variables for records
    number display

--- app/scripts/directives/govtypesmenu.js
+++ app/scripts/directives/govtypesmenu.js
@@ -7,9 +7,20 @@
  * # govTypesMenu
  */
 angular.module('ngCkanApp')
-  .directive('govTypesMenu', function () {
+  .directive('govTypesMenu', function ( ckanService ) {
     return {
-      templateUrl: 'views/gov-types-menu.html',
-      restrict: 'E'
+      templateUrl   : 'views/gov-types-menu.html',
+      restrict      : 'E',
+      link          : function ( scope, element, attrs ) {
+        ckanService.countDatasets( 'vocab_gov_types:Federal' ).then( function ( result ) {
+          scope.gov_federal   = result.count;
+        });
+        ckanService.countDatasets( 'vocab_gov_types:Estatal' ).then( function ( result ) {
+          scope.gov_state     = result.count;
+        });
+        ckanService.countDatasets( 'vocab_gov_types:Municipal' ).then( function ( result ) {
+          scope.gov_municipal = result.count;
+        });
+      }
     };
   });
