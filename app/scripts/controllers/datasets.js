c51278f16fba9ab4892310887a23d8738bcf1529
Carlos Cessa <carlos@pixative.com>
2015-05-21 19:50:44 -0500

    Government Level Filtering
    - Added support to filter datasets results using the government level
    side menu
    - Modified gov-types-menu directive to let the dataset search query
    affect the count for each government level
    - Modified datasets controller to set the search query string in the
    URL in order for the directive to be able to use it
    - Altered directive template to only display the government level when
    there are datasets available for the query

--- app/scripts/controllers/datasets.js
+++ app/scripts/controllers/datasets.js
@@ -7,28 +7,49 @@
  * # DatasetsCtrl
  * Controller of the ngCkanApp
  */
-angular.module('ngCkanApp')
-  .controller('DatasetsCtrl', function ($scope, ckanService) {
+angular.module( 'ngCkanApp' )
+  .controller( 'DatasetsCtrl', function ( $scope, $location, ckanService ) {
+    var query     = "",
+        retrieve  = function () {
+          $scope.searching      = true;
+          ckanService.listDatasets( $scope.start, query ).then( function ( result ) {
+            $scope.datasets     = result.datasets;
+            $scope.resultsCount = result.resultsCount;
+            $scope.searching    = false;
+          });
+        };
 
-    $scope.start = 0;
-
+    $scope.start  = 0;
     $scope.search = function () {
-      var query   = $scope.keyword;
-      if ( !query ) {
-        query = "";
+      // Set the query in the URL search query
+      if ( $scope.keyword ) {
+        $location.search( "search", encodeURIComponent( $scope.keyword ) );
       } else {
-        var exp = query.split(" ").join( "* OR " );
-        query   = query.split(" ").join( " OR " );
-        query   = "title:(" + query + " OR " + exp + "*)";
+        $location.search( "search", null );
+      }
+    };
+
+    $scope.$on( '$routeUpdate', function ( e, route ) {
+      query       = "";
+      // Check if a government level filter is applied
+      if ( route.params.gob ) {
+        var gob = route.params.gob;
+
+        query   += "+vocab_gov_types:(" + gob.charAt( 0 ).toUpperCase() + gob.slice( 1 ) + ")";
+      }
+      // Check if a search query is used
+      if ( route.params.search ) {
+        var search      = decodeURIComponent( route.params.search );
+        $scope.keyword  = search;
+
+        var exp = search.split( " " ).join( "* OR " );
+        search  = search.split( " " ).join( " OR " );
+        query   += "title:(" + search + " OR " + exp + "*)";
       }
 
-      $scope.searching      = true;
-      ckanService.listDatasets( $scope.start, query ).then( function ( result ) {
-        $scope.datasets     = result.datasets;
-        $scope.resultsCount = result.resultsCount;
-        $scope.searching    = false;
-      });
-    }
-
-    $scope.search();
+      if ( !$scope.searching ) {
+        retrieve();
+      }
+    });
+    retrieve();
   });
--- app/scripts/directives/govtypesmenu.js
+++ app/scripts/directives/govtypesmenu.js
@@ -7,20 +7,84 @@
  * # govTypesMenu
  */
 angular.module('ngCkanApp')
-  .directive('govTypesMenu', function ( ckanService ) {
+  .directive('govTypesMenu', function ( $location, ckanService ) {
     return {
       templateUrl   : 'views/gov-types-menu.html',
       restrict      : 'E',
       link          : function ( scope, element, attrs ) {
-        ckanService.countDatasets( 'vocab_gov_types:Federal' ).then( function ( result ) {
-          scope.gov_federal   = result.count;
+        var query   = "",
+            gov     = "",
+            load    = function () {
+              var loadFederal   = true,
+                  loadState     = true,
+                  loadMunicipal = true;
+
+              switch ( gov ) {
+                case "federal" :
+                  scope.gov_state     = 0;
+                  scope.gov_municipal = 0;
+                  loadState           = false;
+                  loadMunicipal       = false;
+                  break;
+                case "estatal" :
+                  scope.gov_federal   = 0;
+                  scope.gov_municipal = 0;
+                  loadFederal         = false;
+                  loadMunicipal       = false;
+                  break;
+                case "municipal" :
+                  scope.gov_federal   = 0;
+                  scope.gov_state     = 0;
+                  loadFederal         = false;
+                  loadState           = false;
+                  break;
+              }
+
+              if ( loadFederal ) {
+                ckanService.countDatasets( query + '+vocab_gov_types:Federal' ).then( function ( result ) {
+                  scope.gov_federal   = result.count;
+                });
+              }
+              if ( loadState ) {
+                ckanService.countDatasets( query + '+vocab_gov_types:Estatal' ).then( function ( result ) {
+                  scope.gov_state     = result.count;
+                });
+              }
+              if ( loadMunicipal ) {
+                ckanService.countDatasets( query + '+vocab_gov_types:Municipal' ).then( function ( result ) {
+                  scope.gov_municipal = result.count;
+                });
+              }
+            };
+
+        scope.$on( '$routeUpdate', function ( e, route ) {
+          query     = "";
+
+          // Check if a search query is used
+          if ( route.params.search ) {
+            var search      = decodeURIComponent( route.params.search );
+
+            var exp = search.split( " " ).join( "* OR " );
+            search  = search.split( " " ).join( " OR " );
+            query   = "title:(" + search + " OR " + exp + "*)";
+          }
+
+          // Check if there's a government level filter set
+          if ( route.params.gob ) {
+            gov     = route.params.gob;
+          } else {
+            gov     = "";
+          }
+
+          load();
         });
-        ckanService.countDatasets( 'vocab_gov_types:Estatal' ).then( function ( result ) {
-          scope.gov_state     = result.count;
-        });
-        ckanService.countDatasets( 'vocab_gov_types:Municipal' ).then( function ( result ) {
-          scope.gov_municipal = result.count;
-        });
+
+        scope.filter  = function ( e, type ) {
+          e.preventDefault();
+
+          $location.search( 'gob', type );
+        };
+        load();
       }
     };
   });
