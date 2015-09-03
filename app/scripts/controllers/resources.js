91a5620dc0a3cfe3f02dab9c8283e551e9dbd8ee
Carlos Cessa <carlos@pixative.com>
2015-09-03 01:38:46 -0500

    Double Hyphen ID
    - Added support to retry and retrieve the resource when the given ID
    has an extra hyphen

--- app/scripts/controllers/resources.js
+++ app/scripts/controllers/resources.js
@@ -9,12 +9,21 @@
  */
 angular.module('ngCkanApp')
   .controller('ResourcesCtrl', function ($scope, $routeParams, ckanService) {
+    var id          = $routeParams.datasetId.replace(/_/g, '-'),
+        retrying    = false,
+        retrieve    = function ( id ) {
+            ckanService.showDataset( id )
+              .then(function(result) {
+                $scope.dataset = result;
+              }, function ( error ) {
+                if ( !retrying ) {
+                  retrying    = true;
+                  id            = id.replace(/--/g, '-');
+                  retrieve( id );
+                }
+              });
+        };
 
-    var id  = $routeParams.datasetId.replace(/_/g, '-');
-    ckanService.showDataset(id)
-      .then(function(result) {
-        $scope.dataset = result;
-      });
-
+    retrieve( id );
   });
 
