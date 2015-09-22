b8074ebdbfe2ba7ef86b342acfecf6dc1250e7cb
Carlos Cessa <carlos@pixative.com>
2015-09-22 15:33:44 -0500

    E2E Tests
    - Added test specs to evaluate the new functionality

--- test/e2e/app.js
+++ test/e2e/app.js
@@ -12,4 +12,42 @@ describe( 'App', function () {
 
         expect( element.all( by.css( 'html.ng-scope' ) ).count() ).toBe( 1 );
     });
+
+    it ( 'should navigate to the second page of the results', function () {
+        browser.get( '/#/conjuntos' );
+
+        element.all( by.css( '.pagination-sm li' ) ).get( 3 ).element( by.tagName( 'a' ) ).click();
+    });
+
+    it ( 'should mantain the page after a refresh and navigate back to the first page', function () {
+        browser.refresh();
+
+        element.all( by.css( '.pagination-sm li' ) ).get( 2 ).element( by.tagName( 'a' ) ).click().then( function () {
+            expect( browser.getLocationAbsUrl() ).toMatch( '/conjuntos' );
+        });
+    });
+
+    it ( 'should filter the search results', function () {
+        element( by.model( 'keyword' ) ).sendKeys( 'Lluvia severa' );
+        browser.actions().sendKeys( protractor.Key.ENTER ).perform();
+    });
+
+    it ( 'should clear the search by sending an empty string', function () {
+        element( by.model( 'keyword' ) ).clear();
+        browser.actions().sendKeys( protractor.Key.ENTER ).perform();
+
+        expect( browser.getLocationAbsUrl() ).toMatch( '/conjuntos' );
+    });
+
+    it ( 'should preserve the search keyword when refreshing the browser', function () {
+        browser.get( '/#/conjuntos?q=presupuesto' );
+
+        element( by.model( 'keyword' ) ).getAttribute( 'value' ).then( function ( value ) {
+            expect( value ).toBe( 'presupuesto' );
+        });
+    });
+
+    it ( 'should clear the search query previously set', function () {
+        element( by.css( '[ng-click="clearSearch()"]' ) ).click();
+    });
 });
