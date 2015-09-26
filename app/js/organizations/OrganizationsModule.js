b5a956937fac7b2c4134c89dc4b3fde7df8696a1
Carlos Cessa <carlos@pixative.com>
2015-09-26 00:37:22 -0500

    Organizations Details
    - Created organizations details state to display the individual
    organization's information
    - Included organization details state, controller and template

--- app/js/organizations/OrganizationsModule.js
+++ app/js/organizations/OrganizationsModule.js
@@ -2,12 +2,15 @@
 
 define( function ( require ) {
     var ResultsCtrl                 = require( 'common/ResultsCtrl' );
+    var OrganizationsDetailsCtrl    = require( 'organizations/OrganizationsDetailsCtrl' );
     var OrganizationsRouter         = require( 'organizations/OrganizationsRouter' );
     var OrganizationsSearchCtrl     = require( 'organizations/OrganizationsSearchCtrl' );
 
     var OrganizationsModule     = angular.module( 'OrganizationsModule', []);
 
     OrganizationsModule.config( [ '$stateProvider', OrganizationsRouter ] );
+
+    OrganizationsModule.controller( 'OrganizationsDetailsCtrl', [ '$scope', '$state', '$stateParams', 'events', 'CkanService', OrganizationsDetailsCtrl ] );
 
     OrganizationsModule.controller( 'OrganizationsResultsCtrl', [ '$scope', '$state', '$stateParams', 'CkanService', ResultsCtrl ] );
 
